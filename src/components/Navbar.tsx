dc7f255d8725e71546e6cf03acf786f3aa042a16
thaparup <thaparup3965@gmail.com>
2025-04-05 20:21:47 +0545

    landing page section completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,13 @@ import Link from 'next/link'
 import MaxWidthWrapper from './MaxWidthWrapper'
 import { ArrowRight } from 'lucide-react'
 import { buttonVariants } from './ui/button'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
 const Navbar = async () => {
+    const { getUser } = getKindeServerSession()
+    const user = await getUser()
     const isAdmin = false
-    const user = undefined
+
     return (
         <nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
             <MaxWidthWrapper>
